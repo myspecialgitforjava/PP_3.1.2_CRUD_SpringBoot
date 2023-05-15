d826db34b4ed309f6113bd1ea3ce22354721efee
Кирилл Крайнов <117298021+myspecialgitforjava@users.noreply.github.com>
2023-05-16 02:12:53 +0300

    minor fixies

--- src/main/java/com/example/project312/model/User.java
+++ src/main/java/com/example/project312/model/User.java
@@ -3,17 +3,17 @@ package com.example.project312.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "uaera")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "first_name")
+    @Column(name = "firstName")
     private String firstName;
-    @Column(name = "last_name")
+    @Column(name = "lastName")
     private String lastName;
-    @Column(name = "user_age")
+    @Column(name = "age")
     private Integer age;
 
     public Long getId() {
